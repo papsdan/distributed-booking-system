450c58af1dbd7b798d1e2dbfc1ffedfd1e99824c
Daniel Papier <dpapier01@student.bbk.ac.uk>
2026-06-08 10:38:58 +0100

    Initial project setup - Included dependencies for Spring Boot, Spring Data JPA, PostgreSQL, Thymeleaf, Spring Security and Lombok

---
+++ src/main/java/com/dpapie01/distributed_booking_system/DistributedBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dpapie01.distributed_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dpapie01/distributed_booking_system/DistributedBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpapie01.distributed_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
