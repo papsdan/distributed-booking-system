e0b0102b45c87eb42f90310270ebe9bfbae358ba
Daniel Papier <dpapier01@student.bbk.ac.uk>
2026-06-26 16:16:38 +0100

    Added UserRepository interface used to interact with db using Spring Data JPA for CRUD operations

---
+++ src/main/java/com/dpapie01/distributed_booking_system/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.dpapie01.distributed_booking_system.repository;
+
+import com.dpapie01.distributed_booking_system.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
