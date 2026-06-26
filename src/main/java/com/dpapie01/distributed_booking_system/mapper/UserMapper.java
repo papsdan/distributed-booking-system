3366bb8f18b68ac1b95cc2b5d4dc22da5faff6ed
Daniel Papier <dpapier01@student.bbk.ac.uk>
2026-06-26 16:58:56 +0100

    Added UserMapper class to convert between the user entity and dto response object for clean architecture

---
+++ src/main/java/com/dpapie01/distributed_booking_system/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.dpapie01.distributed_booking_system.mapper;
+
+import com.dpapie01.distributed_booking_system.dto.UserResponseDTO;
+import com.dpapie01.distributed_booking_system.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public UserResponseDTO toResponseDTO(User user) {
+        UserResponseDTO dto = new UserResponseDTO();
+        dto.setId(user.getId());
+        dto.setFirstName(user.getFirstName());
+        dto.setLastName(user.getLastName());
+        dto.setUsername(user.getUsername());
+        dto.setEmail(user.getEmail());
+        dto.setRole(user.getRole());
+        dto.setCreatedAt(user.getCreatedAt());
+        return dto;
+    }
+}
