a3f4eb2393cd008afda9d97e9c9f468589530966
Daniel Papier <dpapier01@student.bbk.ac.uk>
2026-06-29 13:45:12 +0100

    Added getUserById method to UserService to allow the get request work in UserController.

--- src/main/java/com/dpapie01/distributed_booking_system/service/UserService.java
+++ src/main/java/com/dpapie01/distributed_booking_system/service/UserService.java
@@ -8,6 +8,7 @@ import com.dpapie01.distributed_booking_system.mapper.UserMapper;
 import com.dpapie01.distributed_booking_system.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -17,6 +18,14 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public UserResponseDTO getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return userMapper.toResponseDTO(user);
+    }
 
     public UserResponseDTO register(RegisterRequestDTO dto) {
         if (userRepository.existsByUsername(dto.getUsername())) {
@@ -31,7 +40,7 @@ public class UserService {
         user.setLastName(dto.getLastName());
         user.setUsername(dto.getUsername());
         user.setEmail(dto.getEmail());
-        user.setPassword(dto.getPassword());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
         user.setRole(Role.PLAYER);
         user.setActive(true);
         User savedUser = userRepository.save(user);
