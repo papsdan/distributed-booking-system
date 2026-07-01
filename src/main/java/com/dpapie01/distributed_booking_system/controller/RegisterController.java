4de63e57619bb5d38959885ec90548f87af6a4e0
Daniel Papier <dpapier01@student.bbk.ac.uk>
2026-07-01 15:20:55 +0100

    Added Register Controller for GET/POST requests on /register. Also added the login.html and register.html templates utilising Thymeleaf and Bootstrap for styling: - RegisterController.showRegisterForm uses a GET request to display the registration form - RegisterController.register uses a POST request which validates the submitted form based on the RegisterRequestDTO. Form is reloaded with field errors if there is a failure and redirects user to /login on registration success. - register.html uses Thymeleaf for the registration page. It uses th:field binding for validation error handling on the form and gives inline errors per field so it is understandable for users. - login.html is for the login page. It uses Spring Security out the box filter for verification. It relies on LoginController for error/success messages.

---
+++ src/main/java/com/dpapie01/distributed_booking_system/controller/RegisterController.java
@@ -0,0 +1,48 @@
+package com.dpapie01.distributed_booking_system.controller;
+
+import com.dpapie01.distributed_booking_system.dto.RegisterRequestDTO;
+import com.dpapie01.distributed_booking_system.enums.Gender;
+import com.dpapie01.distributed_booking_system.repository.LocationRepository;
+import com.dpapie01.distributed_booking_system.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@Controller
+@RequestMapping("/register")
+@RequiredArgsConstructor
+public class RegisterController {
+
+    private final UserService userService;
+    private final LocationRepository locationRepository;
+
+    @GetMapping
+    public String showRegisterForm(Model model) {
+        model.addAttribute("registerRequestDto", new RegisterRequestDTO());
+        model.addAttribute("locations", locationRepository.findAll());
+        model.addAttribute("genders", Gender.values());
+        return "register";
+    }
+
+    @PostMapping
+    public String register(@Valid @ModelAttribute("registerRequestDto") RegisterRequestDTO dto, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("locations", locationRepository.findAll());
+            model.addAttribute("genders", Gender.values());
+            return "register";
+        }
+        try {
+            userService.register(dto);
+            return "redirect:/login";
+        } catch (ResponseStatusException e) {
+            model.addAttribute("errorMessage", e.getReason());
+            model.addAttribute("locations", locationRepository.findAll());
+            model.addAttribute("genders", Gender.values());
+            return "register";
+        }
+    }
+}
